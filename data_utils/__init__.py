0d05c311d27beb93f2d19f5689dd6da8cf145cb0
anibalsilva1 <56554802+anibalsilva1@users.noreply.github.com>
2024-11-06 12:57:49 +0000

    Removed unused class inside data_utils.

--- data_utils/__init__.py
+++ data_utils/__init__.py
@@ -1,5 +1,5 @@
 from .datasets import TabularDataset
-from .preprocessors import DataFramePreProcessor, OpenMLCC18DataProcessor
+from .preprocessors import OpenMLCC18DataProcessor
 from .utils import process_output, create_conditional_loaders
 from .dataloaders import create_data_loaders
 from .openmlcc18_utils import get_openmlcc18_datasets_meta_info
